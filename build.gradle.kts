1b370de1c9dd73e7b242a6a064294cb89a53482e
Brendan Abbott <lucidsoftworksllc@gmail.com>
2025-02-20 15:36:21 -0500

    Maintenance: Various Updates
    - Migrated from `org.jetbrains.intellij` to `org.jetbrains.intellij.platform` plugin
      for improved compatibility and latest features.
    - Updated plugin configuration, dependencies, and signing/publishing tasks.
    - Introduced support for IntelliJ Platform's default repositories and artifacts.
    
    Signed-off-by: Brendan Abbott <lucidsoftworksllc@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.1.0"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.2.1"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
 }
 
@@ -10,39 +10,47 @@ version = "1.1.0"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+        localPlatformArtifacts()
+    }
 }
 
-intellij {
-    version.set("2024.3.3")
-    pluginName.set("CLI Actions")
-    plugins.set(listOf("org.jetbrains.plugins.terminal"))
+intellijPlatform {
+    pluginConfiguration {
+        name.set("CLI Actions")
+        version.set("1.1.0")
+        changeNotes.set(
+            """
+            - Update Kotlin
+        """
+        )
+    }
+
+    signing {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishing {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
 }
 
 kotlin {
     jvmToolchain(17)
 }
 
-tasks {
-    patchPluginXml {
-        sinceBuild.set("232")
-        untilBuild.set("")
-        changeNotes.set("""
-            - Update Kotlin
-        """)
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
-}
-
 dependencies {
+    intellijPlatform {
+        bundledPlugin("org.jetbrains.plugins.terminal")
+        jetbrainsRuntime()
+        intellijIdeaCommunity("2024.3.3", false)
+        pluginVerifier()
+        zipSigner()
+    }
+
     implementation("com.charleskorn.kaml:kaml:0.72.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 }

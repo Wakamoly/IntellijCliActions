d1ce97ea9e33cf1a4adad16edabc267dd5dc0f7a
Brendan Abbott <lucidsoftworksllc@gmail.com>
2025-02-20 11:15:22 -0500

    Maintenance: Various Updates
    - Updated Kotlin to version 2.1.0 and Gradle to 8.9 for better compatibility and features.
    - Migrated JVM target and Java settings to JDK 17, improving project consistency.
    - Updated dependencies and adjusted settings in IDEA project files accordingly.
    
    Signed-off-by: Brendan Abbott <lucidsoftworksllc@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
-import org.jetbrains.kotlin.ir.backend.js.compile
-
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.7.20"
-    id("org.jetbrains.intellij") version "1.12.0"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.4.20"
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
 }
 
 group = "io.github.vacxe"
-version = "1.0.6"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
@@ -20,20 +18,28 @@ intellij {
     plugins.set(listOf("org.jetbrains.plugins.terminal"))
 }
 
+kotlin {
+    jvmToolchain(17)
+//    compilerOptions {
+//        jvmTarget = JvmTarget.JVM_17
+//    }
+}
+
 tasks {
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
+//    withType<JavaCompile> {
+//        sourceCompatibility = "17"
+//        targetCompatibility = "17"
+//    }
+//
+//    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+//        kotlinOptions.jvmTarget = "17"
+//    }
 
     patchPluginXml {
         sinceBuild.set("232")
         untilBuild.set("")
         changeNotes.set("""
-            Migrate from deprecated Terminal View. Tab names support and reuse tabs.
+            - Update Kotlin
         """)
     }
 
@@ -49,6 +55,6 @@ tasks {
 }
 
 dependencies {
-    implementation("com.charleskorn.kaml:kaml:0.54.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("com.charleskorn.kaml:kaml:0.72.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 }
